95c7a5bd95188aeeef102e765fc7745069f9e904
Akram Fahim <53183560+akramfahim@users.noreply.github.com>
2019-12-28 18:45:49 +0600

    First Commit
    First Commit

---
+++ js/main.js
@@ -0,0 +1,19 @@
+// //  $(document).ready(function() {
+// //       $(".menu-icon").on("click", function() {
+// //             $("nav ul").toggleClass("showing");
+// //       });
+// // });
+
+// // Scrolling Effect
+
+// $(window).on("scroll", function() {
+//       if($(window).scrollTop()) {
+//             $('header').addClass('black');
+//             $('.subMenu').addClass('black');
+//       }
+
+//       else {
+//             $('header').removeClass('black');
+//             $('.subMenu').removeClass('black');
+//       }
+// })
